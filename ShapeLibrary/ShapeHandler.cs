f7a938b4b531b54a803a42b361422a09067319f2
Alexander Zakharov <ggl_91@mail.ru>
2022-09-06 09:45:25 +0300

    Trial task. Calculating area of shapes logic.

---
+++ ShapeLibrary/ShapeHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace ShapeLibrary
+{
+    public static class ShapeHandler
+    {
+        public static double GetShapeArea(IArea shape)
+        {
+            return shape.GetArea();
+        }
+    }
+}
